452ff9713bd902940b577dbaa3b9d31e062fcda4
Vidya Jegadeesan <vidya016@gmail.com>
2017-09-20 21:14:32 -0500

    Fixed comments.

--- SequenceInPi/PiSequence.pl
+++ SequenceInPi/PiSequence.pl
@@ -1,6 +1,6 @@
 #cerner_2^5_2017
 my $foo = `curl http://www.angio.net/pi/digits/pi1000000.txt`;
-print("Enter in a 5 digit number to locate it in  pi:");
+print("Enter in a 5 digit number to locate it in pi:");
 my $input = <STDIN>;
 chomp $input;
 my $pos = index($foo,$input);
