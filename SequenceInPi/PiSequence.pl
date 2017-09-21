85d61f81d39d51d8f4980e8395e7404ea8d7f434
Vidya Jegadeesan <vidya016@gmail.com>
2017-09-20 20:58:28 -0500

    Perl script to find a sequence of numbers in pi

---
+++ SequenceInPi/PiSequence.pl
@@ -0,0 +1,12 @@
+#cerner_2^5_2017
+my $foo = `curl http://www.angio.net/pi/digits/pi1000000.txt`;
+print("Enter in a 5 digit number to locate it in  pi:");
+my $input = <STDIN>;
+chomp $input;
+my $pos = index($foo,$input);
+if($pos>=0){
+print("$input was found at position $pos");
+}
+else{
+print ("$input sequence not found in the first million digits of pi.")
+}
